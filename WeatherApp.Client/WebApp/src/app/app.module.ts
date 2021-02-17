3c13700168e79f4a29df72043a40c5882ec7febf
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-02-17 19:28:08 +0000

    Added UI components

--- WeatherApp.Client/WebApp/src/app/app.module.ts
+++ WeatherApp.Client/WebApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherTableComponent } from './weather-table/weather-table.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherTableComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule
---
+++ WeatherApp.Client/WebApp/src/app/weather-table/weather-table.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherCard } from '../weather-card/weathercard';
+
+@Component({
+  selector: 'app-weather-table',
+  templateUrl: './weather-table.component.html',
+  styleUrls: ['./weather-table.component.scss']
+})
+export class WeatherTableComponent implements OnInit {
+
+  constructor() {
+    this.weatherCards = new Array(6);
+  }
+
+  ngOnInit(): void {
+  }
+  @Input() weatherCards : Array<WeatherCard>;
+}
